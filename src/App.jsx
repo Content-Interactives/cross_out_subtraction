626e3722c68d2eaccdaf29f24f8cd90c2917eea6
TimothyCruzCk12 <timothy.cruz.intern@ck12.org>
2025-08-19 09:28:28 -0700

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CrossOutSubtraction from './components/CrossOutSubtraction'
+
+
+function App() {
+  return (
+      <div className="App">
+        <CrossOutSubtraction />
+      </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/CrossOutSubtraction.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+
+const CrossOutSubtraction = () => {
+
+
+	return (
+        <div>test</div>
+)
+};
+
+
+export default CrossOutSubtraction;
